fea7d78fd6ec4fb15135e1fbe4d3a9277019dfb0
Nick Singleton <nic.z@me.com>
2022-07-18 13:53:09 +0100

    Initial commit with Next.js 12.2.2

---
+++ shared/components/HelloShared.tsx
@@ -0,0 +1,5 @@
+export default function HelloShared() {
+	return (
+		<h2>Hello Shared!</h2>
+	);
+}
---
+++ website/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import type { NextPage } from 'next';
+
+const Page: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Home</title>
+			</Head>
+
+			<div>
+				<h1>Monorepo Home</h1>
+			</div>
+		</>
+	);
+};
+
+export default Page;
